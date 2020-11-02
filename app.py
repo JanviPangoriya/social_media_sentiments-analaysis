7fb7336d28b117dd198e148dc86bb29c091903cc
Pranav Tomar <pranavtom1223@gmail.com>
2020-11-02 20:42:24 +0530

    in process of deployment

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
+import pandas as pd
 import pickle
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
+
 from nltk import PorterStemmer
 import re
 
