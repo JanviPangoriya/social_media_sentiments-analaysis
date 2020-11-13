88b09248a473a3fa4a246a4ce23afe0ae3b5a34a
JanviPangoriya <janvi.pangoriya_cs18@gla.ac.in>
2020-11-13 18:50:48 +0530

    updated

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
         data = [message]
         vect = cv.transform(data).toarray()
         my_prediction = clf.predict(vect)
-    return render_template('result.html',prediction = my_prediction)
+    return render_template('result.html',tweet = message,prediction = my_prediction)
 
 if __name__ == '__main__':
     app.run(debug=True,use_reloader=False)
